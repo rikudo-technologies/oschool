6a8871f40d57d36d5fad9024317bf7adeb47df27
ChristianMerari <christianmerari7@gmail.com>
2017-05-26 01:32:47 +0200

    Creation de formulaire d'inscription et de connection

---
+++ Bootstrap 2/Index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=">
+	<title>Page d'accueil</title>
+	<link href="bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="stylesheet" type="text/css" href="imagehover.min.css">
+  <link rel="stylesheet" type="text/css" href="font-awesome.min">
+  <link rel="stylesheet" type="text/css" href="bootstrap.js">
+  <link rel="stylesheet" type="text/css" href="jquery.js">
+  <link rel="stylesheet" type="text/css" href="mdb.js">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="index.js">
+</head>
+<body>
+
+<!-- Bar de navigation -->
+<nav class="navbar navbar-white navbar-fixed">
+	<div class="mainHeader_wrapper"></div>
+  <div class="navbar-header page-scroll"></div>
+	<div class="navbar-header">
+		<a class="navbar-brand" href="#">Projet Almanac</a>
+	</div>
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li class="active"> <a href="#">Accueil</a> </li>
+      <li> <a href="#">Parcours</a> </li>
+      <li> <a href="#">Cours</a> </li>
+      <li> <a href="#">Forums</a> </li>
+    </ul>
+    <form class="navbar-form navbar-right inline-form">
+      <div class="form-group">
+        <input type="search" maxlenght="100" size="50" class="input-sm form-control" placeholder="Rechercher">
+        <div style="float: right;padding:0px 50px 14px">
+        <a href="#" class="btn btn-primary btn-danger">s'inscrire</a>
+        <a href="#" class="btn btn-primary btn-success">se connecter</a>
+      </div>
+    </form>
+  </div>
+</nav>
+
+<h1>Faites décoller votre Carrière</h1>
+<p style="font-weight: bold;">Nos parcours 100% diplomants</p>
+<!-- Categorie Formation-->
+
+<section id ="courses" class="section-padding">
+      <div class="container">
+        <div class="row">
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="bac-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="bts-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="ena-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          </div>
+           <div class="col-md-4 center-block">
+            <a href="#" class="btn btn-primary btn-primary" style=" position: absolute; left: 500px; bottom: -80px">Tous les parcours</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+   <section id="work-shop" class="section-padding" style="position: relative; display: block; top: 100px; left: 0px;">
+      <div class="container">
+        <div class="row">
+          <div class="header-section text-center">
+            <h2>Le passeport pour votre réussite</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae,<br> maiores, magni dolorum aliquam.</p>
+            <hr class="bottom-line">
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="service-box text-center">
+              <div class="icon-box">
+                <i class="fa fa-html5 color-green"></i>
+              </div>
+              <div class="icon-text">
+                <h4 class="ser-text">Decrochez Votre Diplomes</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="service-box text-center">
+              <div class="icon-box">
+                <i class="fa fa-css3 color-green"></i>
+              </div>
+              <div class="icon-text">
+                <h4 class="ser-text">Avec des Cours adaptées</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="service-box text-center">
+              <div class="icon-box">
+                <i class="fa fa-joomla color-green"></i>
+              </div>
+              <div class="icon-text">
+                <h4 class="ser-text">Une equipe de qualité</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     <h2 style="text-align: center; position: relative; display: block; top: 110px; left: 0px;">Démarrez avec nos cours ils sont gratuit</h2>
+    </section>
+    <section id ="courses" class="section-padding" style="position: relative; display: block; top: 200px; left: 0px;">
+      <div class="container">
+        <div class="row">
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="bac-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="bts-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          <div class="col-md-4 col-sm-6 padleft-right">
+            <figure class="imghvr-fold-up">
+              <img src="ena-oschool.jpg" class="img-responsive">
+              <figcaption>
+                  <h3>Course Name</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam atque, nostrum veniam consequatur libero fugiat, similique quis.</p>
+              </figcaption>
+              <a href="#"></a>
+            </figure>
+          </div>
+          </div>
+          <!-- Un Mentor pour vous guider -->
+           <section id="faculity-member" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="header-section text-center">
+            <h2>Des mentor pour vous aider</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem nesciunt vitae,<br> maiores, magni dolorum aliquam.</p>
+            <hr class="bottom-line">
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="pm-staff-profile-container" >
+              <div class="pm-staff-profile-image-wrapper text-center">
+                <div class="pm-staff-profile-image">
+                  <img src="Adams.jpg" alt="" class="img-thumbnail img-circle" />
+                </div>   
+              </div>                                
+              <div class="pm-staff-profile-details text-center">  
+                <p class="pm-staff-profile-name">Adams</p>
+                <p class="pm-staff-profile-title">Lead Software Engineer</p>
+                
+                <p class="pm-staff-profile-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et placerat dui. In posuere metus et elit placerat tristique. Maecenas eu est in sem ullamcorper tincidunt. </p>
+              </div>     
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="pm-staff-profile-container" >
+              <div class="pm-staff-profile-image-wrapper text-center">
+                <div class="pm-staff-profile-image">
+                  <img src="Christian.jpg" alt="" class="img-thumbnail img-circle" />
+                </div>   
+              </div>                                
+              <div class="pm-staff-profile-details text-center">  
+                <p class="pm-staff-profile-name">Christian</p>
+                <p class="pm-staff-profile-title">Lead Software Engineer</p>
+                
+                <p class="pm-staff-profile-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et placerat dui. In posuere metus et elit placerat tristique. Maecenas eu est in sem ullamcorper tincidunt. </p>
+              </div>     
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="pm-staff-profile-container" >
+              <div class="pm-staff-profile-image-wrapper text-center">
+                <div class="pm-staff-profile-image">
+                    <img src="Dave3.jpg" alt="" class="img-thumbnail img-circle" />
+                </div>   
+              </div>                                
+              <div class="pm-staff-profile-details text-center">  
+                <p class="pm-staff-profile-name">Dave</p>
+                <p class="pm-staff-profile-title">Lead Software Engineer</p>
+                
+                <p class="pm-staff-profile-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et placerat dui. In posuere metus et elit placerat tristique. Maecenas eu est in sem ullamcorper tincidunt. </p>
+              </div>     
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Témoignage-->
+    <head>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+</head>
+    <div class="container">
+  <div class="row">
+    <div class='col-md-offset-2 col-md-8 text-center'>
+    <h2>Responsive Quote Carousel BS3</h2>
+    </div>
+  </div>
+  <div class='row'>
+    <div class='col-md-offset-2 col-md-8'>
+      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+        <!-- Bottom Carousel Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#quote-carousel" data-slide-to="1"></li>
+          <li data-target="#quote-carousel" data-slide-to="2"></li>
+        </ol>
+        
+        <!-- Carousel Slides / Quotes -->
+        <div class="carousel-inner">
+        
+          <!-- Quote 1 -->
+          <div class="item active">
+            <blockquote>
+              <div class="row">
+                <div class="col-sm-3 text-center">
+                  <img class="img-circle" src="http://www.reactiongifs.com/r/overbite.gif" style="width: 100px;height:100px;">
+                  <!--<img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" style="width: 100px;height:100px;">-->
+                </div>
+                <div class="col-sm-9">
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
+                  <small>Someone famous</small>
+                </div>
+              </div>
+            </blockquote>
+          </div>
+          <!-- Quote 2 -->
+          <div class="item">
+            <blockquote>
+              <div class="row">
+                <div class="col-sm-3 text-center">
+                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/mijustin/128.jpg" style="width: 100px;height:100px;">
+                </div>
+                <div class="col-sm-9">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+                  <small>Someone famous</small>
+                </div>
+              </div>
+            </blockquote>
+          </div>
+          <!-- Quote 3 -->
+          <div class="item">
+            <blockquote>
+              <div class="row">
+                <div class="col-sm-3 text-center">
+                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/keizgoesboom/128.jpg" style="width: 100px;height:100px;">
+                </div>
+                <div class="col-sm-9">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
+                  <small>Someone famous</small>
+                </div>
+              </div>
+            </blockquote>
+          </div>
+        </div>
+        
+        <!-- Carousel Buttons Next/Prev -->
+        <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+        <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+      </div>                          
+    </div>
+  </div>
+</div>
+<hr class="divider"/>
+<script src="index.js"></script>
+
+</body>
+</html>
